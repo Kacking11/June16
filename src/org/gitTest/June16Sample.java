2249290e500e7a0aa2c8913e23dfd67c447d5708
kacking11 <kasiraja16s@gmail.com>
2023-06-16 13:00:16 +0530

    Master code

---
+++ src/org/gitTest/June16Sample.java
@@ -0,0 +1,28 @@
+package org.gitTest;
+
+public class June16Sample {
+	
+	
+	public static void main(String[] args) {
+		
+		String name = "kac";
+		System.out.println(name);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+
+}
